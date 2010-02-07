6f7b9e892cb9750ad6b47817b5ea0e1dbd2e0c5a
James W. Thompson, II <james@plainprograms.com>
2010-02-07 17:02:40 -0600

    Added support for arbitrary separators for thousands places and the decimal point.

--- Fx.CashRegister.js
+++ Fx.CashRegister.js
@@ -29,7 +29,9 @@ Fx.CashRegister = new Class({
   
   options: {
     precision: 2,
-    prefix: '$'
+    prefix: '$',
+    thousandsSeparator: ',',
+    decimalSeparator: '.'
 	},
   
   initialize: function(element, options){
@@ -42,6 +44,7 @@ Fx.CashRegister = new Class({
     
     var current_value = this.element.get('text');
     current_value = current_value.substring(this.options.prefix.length);
+    current_value = this.stripSeparators(current_value);
     current_value = current_value.toFloat().round(this.options.precision);
     
     this.from = current_value;
@@ -57,6 +60,47 @@ Fx.CashRegister = new Class({
   set: function(now){
     var new_value = this.options.prefix + now.round(this.options.precision);
     
-    this.element.set('text', new_value);
+    this.element.set('text', this.addSeparators(new_value));
+  },
+  
+  addSeparators: function(number)
+  {
+  	var parts = number.toString().split(this.options.decimalSeparator);
+  	
+  	var dollars = parts[0];
+  	if (parts[1]) { var cents = parts[1]; }
+  	
+  	var rgx = /(\d+)(\d{3})/;
+  	
+  	while (rgx.test(dollars)) {
+  		dollars = dollars.replace(rgx, '$1' + this.options.thousandsSeparator + '$2');
+  	}
+  	
+  	var output = dollars;
+  	
+  	if (this.options.precision > 0) {
+  	  if (cents) {
+  	    output += this.options.decimalSeparator + cents;
+  	  } else {
+  	    output += '0' * this.options.precision;
+  	  }
+  	}
+  	
+  	return output;
+  },
+  
+  stripSeparators: function(string) {
+    var result = string.replace(this.options.decimalSeparator, '.');
+    var pattern = new RegExp(this.options.thousandsSeparator, 'g');
+    var matches = result.match(pattern)
+    
+    if (matches) {
+      matches.each(function() {
+        result = string.replace(pattern, '');
+      });
+    }
+    
+    return result;
   }
 });
+
